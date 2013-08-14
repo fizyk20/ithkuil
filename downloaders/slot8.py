629ab2f295a0e15720185a10b67f5fbf0aed7bcc
Bartłomiej Kamiński <fizyk20@gmail.com>
2013-08-14 22:02:46 +0200

    Downloaded values for slot VIII.

--- downloaders/slot8.py
+++ downloaders/slot8.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import http.client
-from table_parser import TableParser, reformat, replace_h
+from downloaders.table_parser import TableParser, reformat, replace_h
+from morphology.models import *
 
 sess = http.client.HTTPConnection('ithkuil.net', 80)
 
@@ -45,14 +46,14 @@ def read_table(node):
 				slots = node.tr[i].td[j+1].__data__.split('/')
 				slots = list(map(lambda x: x.replace(' ',''), slots))
 				for s in slots:
-					result.append((case, s))
+					result.append({'case':case, 'slot':s})
 	else:
 		for i in range(len(node.tr)):
 			case = node.tr[i].td[1].__data__
 			slots = node.tr[i].td[3].__data__.split('/')
 			slots = list(map(lambda x: x.replace(' ',''), slots))
 			for s in slots:
-				result.append((case, s))
+				result.append({'case':case, 'slot':s})
 	return result
 
 morph_dict = []
@@ -60,10 +61,38 @@ for node in result2:
 	morph_dict += read_table(node)
 	
 print('Data read.')
-print('Saving to slot8.dat...')
+print('Saving...')
 
-with open('../data/slot8.dat', 'w', encoding='utf-8') as f:
-	for line in morph_dict:
-		f.write('%s: %s\n' % line)
+def value(code, cat):
+	try:
+		res = CategValue.objects.get(code=code)
+	except CategValue.DoesNotExist:
+		res = CategValue(code=code, category=Category.objects.get(name=cat))
+		res.save()
+	return res
+	
+try:
+	formative = WordType.objects.get(name='Formative')
+except WordType.DoesNotExist:
+	formative = WordType(name='Formative')
+	formative.save()
+	
+try:
+	slot = Slot.objects.filter(word_type__name='Formative', number='VIII').get(name='Vc')
+except Slot.DoesNotExist:
+	slot = Slot(number='VIII', name='Vc', word_type=formative)
+	slot.save()
+
+for data in morph_dict:
+	case = value(data['case'], 'Case')
+	
+	try:
+		morph = Morpheme.objects.filter(slot__id=slot.id).get(content=data['slot'])
+	except Morpheme.DoesNotExist:
+		morph = Morpheme(content=data['slot'], slot=slot)
+		morph.save()
+		morph.values.add(case)
+		
+	print(morph.id, ':', morph.content)
 		
 print('Done.')
