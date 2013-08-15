0e3e0d89b08bfdf98ab2a782df00f83d5867b9ef
Bartłomiej Kamiński <fizyk20@gmail.com>
2013-08-15 19:16:12 +0200

    Added word splitter

--- analyzer/views.py
+++ analyzer/views.py
@@ -2,5 +2,18 @@ from django.http import Http404, HttpResponse
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, get_object_or_404
 
+from analyzer.word_splitter import analyze_word
+
 def index(request):
-	return HttpResponse('Hello World!')
+	data = {}
+	if 'sentence' in request.POST:
+		sentence = request.POST['sentence']
+		words = sentence.split()
+		result = []
+		for word in words:
+			slots = analyze_word(word)
+			result.append(str(slots))
+		data['result'] = result
+		return render(request, 'analyzer/index_display.html', data)
+	else:
+		return render(request, 'analyzer/index_form.html', data)
