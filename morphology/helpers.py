329cca83893fa6354e50dbc11f8861287204ecec
Bartłomiej Kamiński <fizyk20@gmail.com>
2014-05-16 18:28:49 +0200

    Added drafts of word classes.

---
+++ morphology/helpers.py
@@ -0,0 +1,33 @@
+vowels = ['a','â','e','ê','ë','i','î','o','ô','ö','u','û','ü',
+		  'á', 'é', 'í', 'ó', 'ú', 'à', 'è', 'ì', 'ò', 'ù']
+
+bare_vowels = ['a', 'e', 'i', 'o', 'u']
+acute_vowels = ['á', 'é', 'í', 'ó', 'ú']
+grave_vowels = ['à', 'è', 'ì', 'ò', 'ù']
+
+consonants_s = ['b', 'c', 'č', 'ç', 'd', 'f', 'g', 'h', 'j', 
+				'k', 'l', 'ļ', 'm', 'n', 'ň', 'p', 'q', 
+				'r', 'ř', 's', 'š', 't', 'ţ', 'v', 'w', 'x', 'y', 'z', 
+				'ż', 'ž']
+consonants_d = ['c’', 'cʰ', 'č’', 'čʰ', 'dh', 'k’', 'kʰ', 'p’', 'pʰ', 
+				'q’', 'qʰ', 't’', 'tʰ', 'xh']
+geminated = ['l', 'm', 'n', 'ň', 'r']
+tones = ['_','/','ˇ','^','¯']
+
+def remove_accents(s):
+	s = s.replace('á', 'a')
+	s = s.replace('é', 'e')
+	s = s.replace('í', 'i')
+	s = s.replace('ó', 'o')
+	s = s.replace('ú', 'u')
+	s = s.replace('à', 'a')
+	s = s.replace('è', 'e')
+	s = s.replace('ò', 'o')
+	s = s.replace('ì', 'i')
+	s = s.replace('ù', 'u')
+	return s
+
+def validation(s):
+	if s in ('h','w','y','hw','hh','hr','hm','hn','lw','ly','rw','ry','řw','řy'):
+		return True
+	return False
--- morphology/word.py
+++ morphology/word.py
@@ -1,1 +1,127 @@
+import abc
+from .helpers import *
 from .data import *
+from morphology import session
+
+def split(s):
+	if not s:
+		return []
+	elif s[0] in tones:
+		return [s[0]] + split(s[1:])
+	elif s[0] in consonants_s + ['’', 'ʰ', '-']:
+		part = ''
+		while s and s[0] in consonants_s + ['’', 'ʰ', '-']:
+			part += s[0]
+			s = s[1:]
+		return [part] + split(s)
+	elif s[0] in vowels + ['’']:
+		part = ''
+		while s and s[0] in vowels + ['’']:
+			part += s[0]
+			s = s[1:]
+		return [part] + split(s)
+	raise Exception('Something went terribly wrong')
+	
+def isPersonalAdjunct(parts):
+	count_consonants = 0
+	for p in parts:
+		if p[0] not in vowels:
+			count_consonants += 1
+	if count_consonants == 1 and parts[-1][0] in vowels:
+		return True
+	if parts[-1][0] in vowels:
+		if parts[-2] in ('w','y','h','hw'):
+			return True
+	else:
+		if '’' in parts[-2] and parts[-3] in ('w','y','h','hw'):
+			return True
+	return False
+
+def isAffixualAdjunct(parts):
+	if len(parts) != 2:
+		return False
+	if parts[0][0] not in vowels or parts[1][0] in vowels:
+		return False
+	return True
+
+def isAspectualAdjunct(parts):
+	if len(parts) != 1:
+		return False
+	if parts[0][0] not in vowels:
+		return False
+	return True
+	
+def isBiasAdjunct(parts):
+	if len(parts) != 1:
+		return False
+	if parts[0][0] in vowels:
+		return False
+	return True
+
+class Word(metaclass=abc.ABCMeta):
+	
+	wordType = None
+	
+	def __init__(self, word):
+		self.word = word
+		self.parts = split(word)
+		self.name = self.wordType.name
+	
+	@staticmethod
+	def fromString(word):
+		parts = split(word)
+		if isPersonalAdjunct(parts):
+			return PersonalAdjunct(word)
+		elif isAffixualAdjunct(parts):
+			return AffixualAdjunct(word)
+		elif isAspectualAdjunct(parts):
+			return AspectualAdjunct(word)
+		elif isBiasAdjunct(parts):
+			return BiasAdjunct(word)
+		else:
+			return Formative(word)
+	
+	@abc.abstractmethod
+	def describe(self):
+		pass
+	
+
+class Formative(Word):
+	
+	wordType = session.query(ithWordType).filter(ithWordType.name == 'Formative').first()
+	
+	def describe(self):
+		return 'Formative'
+	
+
+class PersonalAdjunct(Word):
+	
+	wordType = session.query(ithWordType).filter(ithWordType.name == 'Personal adjunct').first()
+	
+	def describe(self):
+		return 'Personal adjunct'
+	
+
+class AffixualAdjunct(Word):
+	
+	wordType = session.query(ithWordType).filter(ithWordType.name == 'Affixual adjunct').first()
+	
+	def describe(self):
+		return 'Affixual adjunct'
+	
+
+class AspectualAdjunct(Word):
+	
+	wordType = session.query(ithWordType).filter(ithWordType.name == 'Aspectual adjunct').first()
+	
+	def describe(self):
+		return 'Aspectual adjunct'
+	
+
+class BiasAdjunct(Word):
+	
+	wordType = session.query(ithWordType).filter(ithWordType.name == 'Bias adjunct').first()
+	
+	def describe(self):
+		return 'Bias adjunct'
+	
