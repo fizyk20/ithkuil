8408dce01ec7f653b80421a7bfaec2610fd90085
Bartłomiej Kamiński <fizyk20@gmail.com>
2014-05-17 18:10:07 +0200

    Moved Formative class to a separate file.

--- morphology/__init__.py
+++ morphology/__init__.py
@@ -7,3 +7,19 @@ session = Session()
 
 from .data import *
 from .word import *
+from .formative import Formative
+
+def fromString(word):
+	parts = split(word)
+	if isVerbalAdjunct(parts):
+		return VerbalAdjunct(word)
+	elif isPersonalAdjunct(parts):
+		return PersonalAdjunct(word)
+	elif isAffixualAdjunct(parts):
+		return AffixualAdjunct(word)
+	elif isAspectualAdjunct(parts):
+		return AspectualAdjunct(word)
+	elif isBiasAdjunct(parts):
+		return BiasAdjunct(word)
+	else:
+		return Formative(word)
