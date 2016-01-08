18be5ec7e7cb7a5254e5171b394792a24089db63
Bartłomiej Kamiński <fizyk20@gmail.com>
2016-01-08 09:32:29 +0100

    Adapted code to the new database structure

--- ithkuil/morphology/data.py
+++ ithkuil/morphology/data.py
@@ -2,69 +2,74 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Table, Column, Integer, String, Text, ForeignKey
 from sqlalchemy.orm import relationship, backref
 
-__all__ = ['ithCategory', 'ithCategValue', 'ithWordType', 'ithSlot', 'ithMorpheme']
+__all__ = ['ithCategory', 'ithCategValue', 'ithWordType', 'ithSlot', 'ithMorpheme', 'ithMorphemeSlot']
 
 Base = declarative_base()
 
-class ithCategory(Base):
-	'''Class representing a grammatical category'''
-	__tablename__ = 'ith_category'
-	
-	id = Column(Integer, primary_key=True)
-	name = Column(String(128))
-	description = Column(Text)
-	
-
-class ithCategValue(Base):
-	'''Class representing a value of a grammatical category'''
-	__tablename__ = 'ith_categvalue'
-	
-	id = Column(Integer, primary_key=True)
-	code = Column(String(8))
-	name = Column(String(128))
-	description = Column(Text)
-	category_id = Column(Integer, ForeignKey('ith_category.id'))
-	category = relationship('ithCategory')
-	
-
 class ithWordType(Base):
-	'''Class representing a type of a word'''
-	__tablename__ = 'ith_wordtype'
-	
-	id = Column(Integer, primary_key=True)
-	name = Column(String(128))
-	description = Column(Text)
-	regex = Column(String(256))
-	
+    '''Class representing a type of a word'''
+    __tablename__ = 'ith_wordtype'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+    description = Column(Text)
+
+wordtypes_categories = Table('ith_wordtype_category', Base.metadata,
+                        Column('wordtype_id', Integer, ForeignKey('ith_wordtype.id')),
+                        Column('category_id', Integer, ForeignKey('ith_category.id')))
+
+class ithCategory(Base):
+    '''Class representing a grammatical category'''
+    __tablename__ = 'ith_category'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+    description = Column(Text)
+    word_types = relationship('ithWordType', secondary=wordtypes_categories, backref='categories')
 
 class ithSlot(Base):
-	'''Class representing a morphological slot in a word'''
-	__tablename__ = 'ith_slot'
-	
-	id = Column(Integer, primary_key=True)
-	number = Column(String(4))
-	name = Column(String(32))
-	description = Column(Text)
-	regex = Column(String(64))
-	
-	word_type_id = Column(Integer, ForeignKey('ith_wordtype.id'))
-	word_type = relationship('ithWordType', backref=backref('slots', order_by=id))
-	
-
-morpheme_values = Table('ith_morpheme_values', Base.metadata,
-					Column('morpheme_id', Integer, ForeignKey('ith_morpheme.id')),
-					Column('categvalue_id', Integer, ForeignKey('ith_categvalue.id')))
-	
+    '''Class representing a morphological slot in a word'''
+    __tablename__ = 'ith_slot'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(8))
+    description = Column(Text)
+    
+    wordtype_id = Column(Integer, ForeignKey('ith_wordtype.id'))
+    wordtype = relationship('ithWordType', backref='slots')
 
 class ithMorpheme(Base):
-	'''Class representing a morpheme'''
-	__tablename__ = 'ith_morpheme'
-	
-	id = Column(Integer, primary_key=True)
-	content = Column(String(8))
-	tone = Column(String(16))
-	stress = Column(String(16))
-	slot_id = Column(Integer, ForeignKey('ith_slot.id'))
-	slot = relationship('ithSlot', backref=backref('morphemes', order_by=id))
-	values = relationship('ithCategValue', secondary=morpheme_values, backref='morphemes')
+    '''Class representing a morpheme'''
+    __tablename__ = 'ith_morpheme'
+    
+    id = Column(Integer, primary_key=True)
+    morpheme = Column(String(8))
+
+class ithMorphemeSlot(Base):
+    __tablename__ = 'ith_morpheme_slot'
+
+    id = Column(Integer, primary_key=True)
+    morpheme_id = Column(Integer, ForeignKey('ith_morpheme.id'))
+    slot_id = Column(Integer, ForeignKey('ith_slot.id'))
+
+    morpheme = relationship('ithMorpheme', backref='slots')
+    slot = relationship('ithSlot', backref='morphemes')
+    
+
+morpheme_slots_values = Table('ith_morpheme_slots_values', Base.metadata,
+                        Column('morpheme_slot_id', Integer, ForeignKey('ith_morpheme_slot.id')),
+                        Column('categvalue_id', Integer, ForeignKey('ith_categvalue.id')))
+    
+
+class ithCategValue(Base):
+    '''Class representing a value of a grammatical category'''
+    __tablename__ = 'ith_categvalue'
+    
+    id = Column(Integer, primary_key=True)
+    code = Column(String(8))
+    name = Column(String(128))
+    description = Column(Text)
+    category_id = Column(Integer, ForeignKey('ith_category.id'))
+    category = relationship('ithCategory')
+    morpheme_slots = relationship('ithMorphemeSlot', secondary=morpheme_slots_values, backref='values')
 	
