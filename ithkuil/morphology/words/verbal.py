2a4146b7e6e47044b080d6341b9f948026ad180e
Bartłomiej Kamiński <fizyk20@gmail.com>
2016-01-09 12:56:21 +0100

    Added abbreviated description for verbal adjuncts for testing purposes

--- ithkuil/morphology/words/verbal.py
+++ ithkuil/morphology/words/verbal.py
@@ -13,29 +13,65 @@ class VerbalAdjunct(Word):
     
         if len(parts)>2 and parts[-2][-1] == '’':
             parts[-2] = parts[-2][:-1]
-            self._slots['G'] = parts[-1]
+            self._slots['Cb'] = parts[-1]
             parts = parts[:-1]
         
         if parts[-1][0] in vowels:
-            self._slots['F'] = parts[-1]
+            self._slots['Vs'] = parts[-1]
             parts = parts[:-1]
             
         if parts[0] in tones:
-            self._slots['H'] = parts[0]
+            self._slots['[tone]'] = parts[0]
             parts = parts[1:]
             
-        self._slots['E'] = parts[-1]
+        self._slots['Cs'] = parts[-1]
         if len(parts) > 1:
-            self._slots['D'] = parts[-2]
+            self._slots['Vm'] = parts[-2]
         if len(parts) > 2:
-            self._slots['C'] = parts[-3]
+            self._slots['Cv'] = parts[-3]
         if len(parts) > 3:
-            self._slots['B'] = parts[-4]
+            self._slots['Ve'] = parts[-4]
         if len(parts) > 4:
-            self._slots['A'] = parts[-5]
+            self._slots['Cl'] = parts[-5]
+            
+    def fillResult(self, add):
+        add('Cl')
+        add('Ve')
+        add('Cv')
+        add('Vm')
+        add('Cs')
+        add('Vs')
+        add('Cb')
+        add('[tone]')
     
     def abbreviatedDescription(self):
-        return 'Verbal adjunct'
+        desc = []
+        
+        def values(morph):
+            if isinstance(morph, str):
+                return morph
+            vals = [x.code for x in morph.values]
+            return '/'.join(vals)
+        
+        def add(slot):
+            # handle biases
+            if slot == 'Cb' and slot in self.slots:
+                val = self.slots[slot]
+                if len(val) > 1 and val[-1] == val[-2]:
+                    val = (val[:-1], True)
+                elif val == 'xxh':
+                    val = ('xh', True)
+                else:
+                    val = (val, False)
+                desc.append('%s%s' % (self.morpheme(slot, val[0]).values[0].code, '+' if val[1] else ''))
+                return
+            
+            if slot in self.slots:
+                desc.append(values(self.morpheme(slot, self.slots[slot])))
+            
+        self.fillResult(add)
+        
+        return '-'.join(desc)
     
     def fullDescription(self):
         return {'type': 'Verbal adjunct', 'categories': []}
