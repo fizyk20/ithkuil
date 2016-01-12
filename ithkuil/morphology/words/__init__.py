b6f41ee971449ea26cb4efe0e04e723d286e0591
Bartłomiej Kamiński <fizyk20@gmail.com>
2016-01-12 14:34:46 +0100

    Add stress removal to word factory

--- ithkuil/morphology/words/__init__.py
+++ ithkuil/morphology/words/__init__.py
@@ -8,6 +8,7 @@ from .affixual import AffixualAdjunct
 from .aspectual import AspectualAdjunct
 from .bias import BiasAdjunct
 from ..helpers import handle_special_chars, filter_chars
+import re
 
 class Factory(IthkuilVisitor):
     
@@ -40,6 +41,42 @@ class Factory(IthkuilVisitor):
         word = visit_parse_tree(node, CombineVisitor())
         result = super().visit_formative(node, children)
         return Formative(word, result)
+        
+    def visit_vowels(self, node, children):
+        '''This visiting function will remove stress marks from the vowels'''
+        temp = ''.join(children)
+        
+        def remove_accents(m):
+            s = m.group(0)
+            return s.replace('á', 'a')\
+                    .replace('é', 'e')\
+                    .replace('í', 'i')\
+                    .replace('ó', 'o')\
+                    .replace('ú', 'u')\
+                    .replace('à', 'a')\
+                    .replace('è', 'e')\
+                    .replace('ò', 'o')\
+                    .replace('ì', 'i')\
+                    .replace('ù', 'u')
+        
+        stressed = {
+            r'([ëöüâêîôû])\1': r'\1',
+            'áu': 'au',
+            'éu': 'eu',
+            'íu': 'iu',
+            'óu': 'ou',
+            '[áéó]': remove_accents,
+            '[àèò]': remove_accents,
+            '[^aeiou][ìù]': remove_accents,
+            '[^aeiou][íú]': remove_accents,
+            '([aeou])í': r'\1ì',
+            '([aeio])ú': r'\1ù'
+        }
+        
+        for k, v in stressed.items():
+            temp = re.sub(k, v, temp)
+            
+        return temp
     
     @classmethod
     def parseWord(cls, word):
